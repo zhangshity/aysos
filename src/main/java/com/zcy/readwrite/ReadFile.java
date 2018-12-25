93a27f178bfcfdee0d3c606c847d4bc83b4d0767
chunyang.zhang <525768181@qq.com>
2018-12-25 14:16:56 +0800

    read & write files .The use of String type . 读写文件，String相关类型的使用

---
+++ src/main/java/com/zcy/readwrite/ReadFile.java
@@ -0,0 +1,44 @@
+//package com.zcy.readwrite;
+//
+//import com.zcy.readwrite.entity.Student;
+//
+//import java.io.BufferedReader;
+//import java.io.FileNotFoundException;
+//import java.io.FileReader;
+//import java.util.ArrayList;
+//
+///**
+// * @ Author: chunyang.zhang
+// * @ Description:
+// * @ Date: Created in 17:27 2018-12-21
+// * @ Modified: By:
+// */
+//
+//public class ReadFile {
+//
+//
+//    public ArrayList getRecord() {
+//        ArrayList<Student> arrayList = new ArrayList<Student>();
+//
+//        //绝对路径
+////    BufferedReader bf = new BufferedReader(
+////            new FileReader("./Users/zhangchunyang/Documents/IdeaProjects/aysos/src/main/resources"));
+////
+//        //相对路径
+//        try {
+//            BufferedReader bf = new BufferedReader(
+//                    new FileReader("./aysos/src/main/resources/record.txt"));
+//
+//            bf.readLine();
+//
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//
+//    }
+//
+//
+//}
