0e4436fe34e7869ec9eecf04167ccfa769a754d9
chunyang.zhang <525768181@qq.com>
2019-01-28 00:48:57 +0800

    DecimalFormat / bigDcimal.toString()

---
+++ src/main/java/com/zcy/tools/decimal_format/BigDecimal2String.java
@@ -0,0 +1,19 @@
+package com.zcy.tools.decimal_format;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BigDecimal2String {
+
+    public static void main(String[] args) {
+
+        BigDecimal bigDecimal = new BigDecimal(1233.12);
+
+        System.out.println(bigDecimal);
+
+        System.out.println(bigDecimal.toString());
+
+        List list = new ArrayList();
+    }
+}
