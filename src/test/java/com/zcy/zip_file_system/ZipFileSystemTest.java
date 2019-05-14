058f10c362f2cd92e8e70cd0708a3c7c25737b5d
chunyang.zhang <525768181@qq.com>
2019-05-14 15:26:34 +0800

    压缩文件系统测试 zip

---
+++ src/test/java/com/zcy/zip_file_system/ZipFileSystemTest.java
@@ -0,0 +1,15 @@
+package com.zcy.zip_file_system;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ZipFileSystemTest {
+
+    @Test
+    public void testMethod() {
+        ZipFileSystem zipFileSystem = new ZipFileSystem();
+        zipFileSystem.testMethod();
+    }
+
+}
