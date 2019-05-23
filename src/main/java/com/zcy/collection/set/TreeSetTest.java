452c64b19ccbe51498e8c5c3a74f815740e06f1e
chunyang.zhang <525768181@qq.com>
2019-05-23 17:33:28 +0800

    TreeSet测试

--- src/main/java/com/zcy/collection/set/TreeSetTest.java
+++ src/main/java/com/zcy/collection/set/TreeSetTest.java
@@ -1,4 +1,46 @@
 package com.zcy.collection.set;
 
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @ Author: chunyang.zhang
+ * @ Description:
+ * @ Date: Created in 16:41 2019-05-22
+ * @ Modified: By:
+ */
 public class TreeSetTest {
+
+
+    public static void main(String[] args) {
+        TreeSetTest treeSetTest = new TreeSetTest();
+        treeSetTest.testDemo();
+    }
+
+    public void testDemo() {
+        Set<String> set = new TreeSet();
+        set.add("123");
+        set.add("1asd");
+        set.add("2sdsfs");
+
+        //foreach遍历
+        for (String s : set) {
+            System.out.println(s);
+        }
+
+        //常规遍历
+        for (int i = 0; i < set.size(); i++) {
+            Object[] o = set.toArray();
+            System.out.println(o[i]);
+        }
+
+        //Iterator遍历
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            String value = iterator.next();
+            System.out.println(value);
+        }
+    }
+
 }
