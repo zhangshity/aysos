e1fab2a1b309453584f42e22c7c66917c903b034
chunyang.zhang <525768181@qq.com>
2019-07-22 17:34:11 +0800

    Value of Global Variable and Local Variable (全局变量和局部变量赋值问题)

---
+++ src/main/java/com/zcy/variable/VariableValue.java
@@ -0,0 +1,30 @@
+package com.zcy.variable;
+
+/**
+ * @ Author: chunyang.zhang
+ * @ Description: 《测试全局变量&局部变量赋值》 <Global Variable & Local Variable/>
+ * @ Date: Created in 17:12 2019-07-22
+ * @ Modified: By:
+ */
+public class VariableValue {
+
+    //全局变量
+    private static String globalVariable_NonValue;
+    private static String globalVariable_HasValue = "global_value";
+
+
+    public static void main(String[] args) {
+        //局部变量
+        String localVariable_NonValue;
+        String localVariable_HasValue = "";
+
+
+        //全局变量打印
+        System.out.println("全局变量(未赋初值): " + globalVariable_NonValue);
+        System.out.println("全局变量(赋初值): " + globalVariable_HasValue);
+        //局部变量打印
+//        System.out.println("局部变量(未赋初值): " + localVariable_NonValue);//不赋初值根本没法调用(编译不通过)
+        System.out.println("局部变量(赋初值): " + localVariable_HasValue);
+
+    }
+}
