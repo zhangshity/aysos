a4d4288813836ebb0c19270b7b3fa986595c4baa
chunyang.zhang <525768181@qq.com>
2019-07-29 14:58:53 +0800

    shadow clone 深克隆案例修改。关联github .md笔记

--- src/main/java/com/zcy/clone/internetexample/Professor.java
+++ src/main/java/com/zcy/clone/internetexample/Professor.java
@@ -1,9 +1,17 @@
 package com.zcy.clone.internetexample;
 
+/**
+ * @ Author: chunyang.zhang
+ * @ Description: 《深克隆-对象类》<ShadowCopy-object/>
+ * @ Date: Created in 14:47 2019-07-29
+ * @ Modified: By:
+ * <p>
+ * 参考对应Github文档:
+ * https://github.com/zhangshity/technote/blob/master/Java/clone()%E6%96%B9%E6%B3%95.md
+ */
 public class Professor {
 
     private String name;
-
     private int age;
 
     public String getName() {
