d295a997150da2ea757ba6a7b97d99e9bf208ebd
chunyang.zhang <525768181@qq.com>
2019-09-05 17:10:56 +0800

    maven插件开发demo

---
+++ src/main/java/com/zcy/maven/package-info.java
@@ -0,0 +1,2 @@
+
+package com.zcy.maven;
---
+++ src/main/java/com/zcy/maven/plugin/GreetingMojo.java
@@ -0,0 +1,17 @@
+package com.zcy.maven.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "sayhi")
+public class GreetingMojo extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Hello, world.");
+    }
+
+
+}
