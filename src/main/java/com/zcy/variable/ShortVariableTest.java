93328c971cc512f9e3ad9e1b450bf64e3b2ac5b7
chunyang.zhang <525768181@qq.com>
2019-10-23 16:14:04 +0800

    short 面试题：1.初始化问题 2.类型不匹配不能运算问题

--- src/main/java/com/zcy/variable/ShortVariableTest.java
+++ src/main/java/com/zcy/variable/ShortVariableTest.java
@@ -1,4 +1,27 @@
 package com.zcy.variable;
 
-public class ShortTest {
+/**
+ * @ Author: chunyang.zhang
+ * @ Description:
+ * @ Date: Created in 14:33 2019/10/23
+ * @ Modified: By:
+ * <p>
+ * 根据一傻逼公司的傻逼面试题的回忆，测试
+ */
+public class ShortVariableTest {
+
+    public static void main(String[] args) {
+        //1.无法通过编译，未赋初值
+//        short s1;
+//        s1 = s1 + 1;
+
+        //2.short类型和int不匹配
+//        short s2 = 1;
+//        s2 = s2 + 1;
+
+        //无错
+        short s3 = 1;
+        s3 += s3;
+
+    }
 }
