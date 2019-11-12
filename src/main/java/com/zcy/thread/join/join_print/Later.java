f9fae92523f5934068109958e68aa6965d801167
chunyang.zhang <525768181@qq.com>
2019-11-12 14:02:47 +0800

    多线程-join()实现线程条件先后运行

---
+++ src/main/java/com/zcy/thread/join/join_print/Later.java
@@ -0,0 +1,14 @@
+package com.zcy.thread.join.join_print;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Later implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i + " " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+
+        }
+    }
+}
