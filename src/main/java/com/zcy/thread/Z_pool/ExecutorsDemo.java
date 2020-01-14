46d4aa53e5e962dbeca66fb21b4966ac25d236b4
chunyang.zhang <525768181@qq.com>
2020-01-14 16:07:41 +0800

    ReentrantLock 重入锁的简单使用 lock() & unlock()

---
+++ src/main/java/com/zcy/thread/Z_pool/ExecutorsDemo.java
@@ -0,0 +1,17 @@
+package com.zcy.thread.Z_pool;
+
+public class ExecutorsDemo {
+    public static void main(String[] args) {
+
+//        Thread t = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("新线程" + new SimpleDateFormat("YY-MM-DD HH:mm:ss"));
+//            }
+//        });
+
+//        ExecutorService executor = new
+
+//        ThreadPoolExecutor
+    }
+}
