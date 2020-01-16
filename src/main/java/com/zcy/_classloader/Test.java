7d7b42ba1ebd8fdc3638aa13bd96764a4da2b9c4
chunyang.zhang <525768181@qq.com>
2020-01-16 10:46:38 +0800

    ClassLoader load path (加载路径)

--- src/main/java/com/zcy/_classloader/Test.java
+++ src/main/java/com/zcy/_classloader/Test.java
@@ -1,5 +1,7 @@
 package com.zcy._classloader;
 
+import sun.applet.AppletClassLoader;
+
 public class Test {
     public static void main(String[] args) {
         //Bootstrap ClassLoader
@@ -12,5 +14,6 @@ public class Test {
 
         //
 //        ClassLoader
+        AppletClassLoader
     }
 }
