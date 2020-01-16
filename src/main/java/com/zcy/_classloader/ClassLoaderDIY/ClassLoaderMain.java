5fa745efd639eb585296af8690a150976afeee07
chunyang.zhang <525768181@qq.com>
2020-01-16 10:47:22 +0800

    ClassLoader DIY (自定义ClassLoader)

---
+++ src/main/java/com/zcy/_classloader/ClassLoaderDIY/ClassLoaderMain.java
@@ -0,0 +1,10 @@
+package com.zcy._classloader.ClassLoaderDIY;
+
+public class ClassLoaderMain {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        MyClassLoader myClassLoader = new MyClassLoader("/Users/zhangchunyang/Desktop/", "myClassLoader");
+        Class c = myClassLoader.loadClass("Student");
+        System.out.println(c.getClassLoader());
+        c.newInstance();
+    }
+}
---
+++ src/main/java/com/zcy/_classloader/ClassLoaderDIY/MyClassLoader.java
@@ -0,0 +1,50 @@
+package com.zcy._classloader.ClassLoaderDIY;
+
+import java.io.*;
+
+public class MyClassLoader extends ClassLoader {
+
+    private String path;
+    private String classLoaderName;
+
+    public MyClassLoader(String path, String classLoaderName) {
+        this.path = path;
+        this.classLoaderName = classLoaderName;
+    }
+
+    //寻找类文件
+    @Override
+    public Class findClass(String name) {
+        byte[] b = loadClassData(name);
+        return defineClass(name, b, 0, b.length);
+    }
+
+    //用于加载类文件
+    private byte[] loadClassData(String name) {
+        name = path + name + ".class";
+        InputStream in = null;
+        ByteArrayOutputStream out = null;
+        try {
+            in = new FileInputStream(new File(name));
+            out = new ByteArrayOutputStream();
+            int i = 1;
+            while ((i = in.read()) != -1) {
+                out.write(i);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return out.toByteArray();
+    }
+
+
+}
