21086ebbf13e1c7196891358b6b02f8a58f8d2fc
chunyang.zhang <525768181@qq.com>
2020-03-11 01:44:20 +0800

    Introdution <aop:declare-parents  types-matching=" "  implement-interface=" "  default-impl=" "/>

---
+++ src/main/java/com/zcy/spring/aop/demo3_introduction_xml/BusinessClass.java
@@ -0,0 +1,9 @@
+package com.zcy.spring.aop.demo3_introduction_xml;
+
+public class BusinessClass {
+
+    public void biz() {
+        System.out.println("BusinessClass.biz()");
+    }
+
+}
---
+++ src/main/java/com/zcy/spring/aop/demo3_introduction_xml/Fit.java
@@ -0,0 +1,6 @@
+package com.zcy.spring.aop.demo3_introduction_xml;
+
+public interface Fit {
+
+    void filter();
+}
