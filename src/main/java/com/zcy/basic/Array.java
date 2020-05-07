bb6e305c8376f44e2a97c9f6782316cb03652396
chunyang.zhang <525768181@qq.com>
2020-05-07 23:49:16 +0800

    数组不赋值默认值为零

---
+++ src/main/java/com/zcy/basic/Array.java
@@ -0,0 +1,14 @@
+package com.zcy.basic;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] i = new int[5]; //不符值默认都是零
+        System.out.println(i[0]);
+        System.out.println(i[1]);
+        System.out.println(i[2]);
+        System.out.println(i[3]);
+        System.out.println(i[4]);
+    }
+
+
+}
