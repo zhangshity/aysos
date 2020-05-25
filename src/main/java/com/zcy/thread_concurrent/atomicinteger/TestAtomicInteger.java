6fc686f523ae21d46a6927b25fbd4a490eb6097a
chunyang.zhang <525768181@qq.com>
2020-05-26 00:02:05 +0800

    AtomicInteger原子操作

--- src/main/java/com/zcy/thread_concurrent/atomicinteger/TestAtomicInteger.java
+++ src/main/java/com/zcy/thread_concurrent/atomicinteger/TestAtomicInteger.java
@@ -1,4 +1,38 @@
 package com.zcy.thread_concurrent.atomicinteger;
 
-public class M {
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TestAtomicInteger {
+    private static final int THREADS_COUNT = 2;
+
+    public static int count = 0;
+    public static volatile int countVolatile = 0;
+    public static AtomicInteger atomicInteger = new AtomicInteger(0);
+    public static CountDownLatch countDownLatch = new CountDownLatch(2);
+
+    public static void increase() {
+        count++;
+        countVolatile++;
+        atomicInteger.incrementAndGet();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[THREADS_COUNT];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> {
+                for (int i1 = 0; i1 < 1000; i1++) {
+                    increase();
+                }
+                countDownLatch.countDown();
+            });
+            threads[i].start();
+        }
+
+        countDownLatch.await();
+
+        System.out.println(count);
+        System.out.println(countVolatile);
+        System.out.println(atomicInteger.get());
+    }
 }
