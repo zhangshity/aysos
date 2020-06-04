1edc2d5edaa556dbf1c56d1ccc15cdbc358d8a47
chunyang_zhang <525768181@qq.com>
2020-06-04 16:20:54 +0800

    日期比较的三种方法

---
+++ src/main/java/com/zcy/time/DateCompare.java
@@ -0,0 +1,27 @@
+package com.zcy.time;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateCompare {
+    public static void main(String[] args) throws ParseException {
+        String dateStr1 = "2020-05-26";
+        String dateStr2 = "2020-05-26";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date1 = simpleDateFormat.parse(dateStr1);
+        Date date2 = simpleDateFormat.parse(dateStr2);
+        Long dateLong1 = date1.getTime();
+        Long dateLong2 = date2.getTime();
+
+        System.out.println(date1);
+        System.out.println(date2);
+        System.out.println(dateLong1);
+        System.out.println(dateLong2);
+
+        System.out.println(dateStr1.compareTo(dateStr2));
+        System.out.println(dateLong1 <= dateLong2);
+        System.out.println(date1.before(date2));
+        System.out.println(date1.after(date2));
+    }
+}
