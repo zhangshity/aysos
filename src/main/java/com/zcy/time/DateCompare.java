fe4a83c01405144efb6a2ccef388a4b89e517f2a
chunyang_zhang <525768181@qq.com>
2020-06-20 10:25:00 +0800

    Date类型的 comparaTo方法

--- src/main/java/com/zcy/time/DateCompare.java
+++ src/main/java/com/zcy/time/DateCompare.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public class DateCompare {
     public static void main(String[] args) throws ParseException {
         String dateStr1 = "2020-05-26";
-        String dateStr2 = "2020-05-26";
+        String dateStr2 = "2019-11-28";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date date1 = simpleDateFormat.parse(dateStr1);
         Date date2 = simpleDateFormat.parse(dateStr2);
@@ -20,6 +20,7 @@ public class DateCompare {
         System.out.println(dateLong2);
 
         System.out.println(dateStr1.compareTo(dateStr2));
+        System.out.println(date1.compareTo(date2));
         System.out.println(dateLong1 <= dateLong2);
         System.out.println(date1.before(date2));
         System.out.println(date1.after(date2));
