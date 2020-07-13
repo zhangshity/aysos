fd0f5aa2882442973ceccc332280087608f2fbb0
chunyang.zhang <525768181@qq.com>
2020-07-13 22:50:06 +0800

    bubbleSort review

--- src/main/java/com/zcy/DataStructures_Algorithms/algorithms/sort/BubbleSort.java
+++ src/main/java/com/zcy/DataStructures_Algorithms/algorithms/sort/BubbleSort.java
@@ -9,19 +9,21 @@ public class BubbleSort {
      * 冒泡排序
      * <p>
      * 1.进行多轮即(循环轮数为数组长度)
-     * 2.每轮中,从第一个数开始,与右边的数字比较,大于右边数字就交换,否则不变
-     * ........从第二个数开始,与右边的数字比较,大于右边数字就交换,否则不变
-     * ........从第三个数开始,与右边的数字比较,大于右边数字就交换,否则不变
-     * ........从第四个数开始,与右边的数字比较,大于右边数字就交换,否则不变
+     * 2.每轮中,第一个数字,与右边的数字(第二个数字)比较,大于右边数字就交换,否则不变
+     * ........第二个数字,与右边的数字(第三个数字)比较,大于右边数字就交换,否则不变
+     * ........第三个数字,与右边的数字(第四个数字)比较,大于右边数字就交换,否则不变
+     * ........第四个数字,与右边的数字(第五个数字)比较,大于右边数字就交换,否则不变
      * ...
-     * ........从第n个数开始,与右边的数字比较,大于右边数字就交换,否则不变(n=arr.length-1)
+     * ........第n个数字,与右边的数字比较,大于右边数字就交换,否则不变(n=arr.length-1)
      * <p>
      * <p>
-     * 3.每次都会把一个最大数字放到最末尾,即下次左右比较的次数减一,第i轮循环次数为数组长度减i
-     * 最后一次左右比较,右边的j+1小于数组长度 (j+1 = arr.length-i) 故 (j=arr.length-i-1)
+     * 3.每次都会把一个最大数字放到最末尾,即下次左右比较的次数减一,第n轮(i+1)循环比较次数为数组长度减n(arr.length-(i+1))
+     * - > (i从0开始,而模型n从1开始故n=i+1)
+     * <p>
+     * 另一种理解方式:最后一次左右比较,右边的j+1小于数组长度 (j+1 = arr.length-i) 故 (j=arr.length-i-1)
      *
-     * @param arr
-     * @return
+     * @param arr 整型数组
+     * @see com.zcy.leetcode.BubbleSortAndTuning#bubbleSort(Integer[])
      */
     public static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) { //控制循轮数
@@ -40,9 +42,11 @@ public class BubbleSort {
     /**
      * 冒泡排序优化
      * <p>
-     * 上述排序,第四轮就完成了排序,故加入标准为判断是否有左右交换,无则退出排序
+     * 上述排序,第四轮就完成了排序,但是程序依然在进行比较，故需要优化。
+     * Tuning: 在外层for控制循环次数层内,加入标志<code>boolean</code>flag(判断有无左右交换),无交换break出循环
      *
-     * @param arr
+     * @param arr 整型数组
+     * @see com.zcy.leetcode.BubbleSortAndTuning#bubbleSortTuning(Integer[])
      */
     public static void tuningBubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
