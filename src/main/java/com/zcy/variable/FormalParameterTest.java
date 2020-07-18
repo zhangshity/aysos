145f0d1804521cc767509445e413b0cce1687819
chunyang.zhang <525768181@qq.com>
2020-07-18 22:37:02 +0800

    关于形参和实参的区别深入理解 - 经典问题

--- src/main/java/com/zcy/variable/FormalParameterTest.java
+++ src/main/java/com/zcy/variable/FormalParameterTest.java
@@ -30,14 +30,13 @@ public class FormalParameterTest {
     /**
      * 【Q&A】-问题：
      * Q:
-     * System.identityHashCode()；是根据内存地址计算得出的哈希码，内存地址不一样，得出的哈希码也不一样
-     * 可以看出a的地址和c的地址竟然是一样的？
+     * System.identityHashCode()；是根据内存地址计算得出的哈希码，内存地址不一样，得出的哈希码也不一样，可以看出a的地址和c的地址竟然是一样的？
      * 可是书上明明说的是，形参属于局部变量，当程序运行到该方法时，会为形参分配栈内存。既然重新分配了内存，那么为什么a和c的地址还会是一样的？
      *
      * A:
-     * 首先，hashcode()方法是object方法，identityHashCode（object X）,传入int值会被自动转成Integer类型。
-     *                                (即基本变量无法使用这个方法拿到内存地址,只有引用才可以通过hashcode拿到其对象的内存地址)
-     * 其次，类加载时，字面量会被纳入常量池中，包括String常量和Integer -128---127。 c确实是形参，
+     * 首先，hashCode()方法是Object类的方法，System.identityHashCode(Object X)返回值与其相同，传入int值会被自动装箱成Integer类型。
+     *      (即基本变量无法使用hashCode()方法拿到内存地址,只有引用才可以通过hashCode()拿到其对象的内存地址)
+     * 其次，包装类加载时，其值会被纳入常量池中缓存，包括String常量和Integer (-128-127)。 c确实是形参，但是
      *      a,c的System.identityHashCode()却是对象Integer.valueOf(6),输出的地址自然就是一样的。你可以把6换成600，就不会在创建在常量池了，而是在堆中，这样地址就不可能一样了。
      */
 
