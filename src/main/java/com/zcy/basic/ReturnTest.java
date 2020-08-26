d80b4b6dd86d88ce7aeef37da3d11acfbb80115c
Allen.C.Y.Zhang <allenzhang@oceanpayment.com.cn>
2020-08-26 14:56:16 +0800

    返回值赋值测试  先做运算再返回

---
+++ src/main/java/com/zcy/basic/ReturnTest.java
@@ -0,0 +1,17 @@
+package com.zcy.basic;
+
+public class ReturnTest {
+
+    public static void main(String[] args) {
+        System.out.println(value());
+    }
+
+    public static int value() {
+        int num = 1;
+        return num = 2;
+    }
+
+
+
+
+}
