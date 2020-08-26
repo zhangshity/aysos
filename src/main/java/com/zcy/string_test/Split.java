5a97a6bd80af61cfd1fba12a6461dee583810c8f
Allen.C.Y.Zhang <allenzhang@oceanpayment.com.cn>
2020-08-26 14:57:14 +0800

    String 类   split方法的 边界问题(分隔符前解析空串""   尾舍弃)

--- src/main/java/com/zcy/string_test/Split.java
+++ src/main/java/com/zcy/string_test/Split.java
@@ -1,5 +1,8 @@
 package com.zcy.string_test;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @ Author: chunyang.zhang
  * @ Description:
@@ -26,5 +29,28 @@ public class Split {
         System.out.println(beginDateTime.split(" ")[0]);
         System.out.println(endDateTime.split(" ")[0]);
         System.out.println(beginDateTime.split(" ")[0].equals(endDateTime.split(" ")[0]));
+
+
+        //================================2020-08-26 14:14:?? Wen.
+        System.out.println("//===========边界测试=====================2020-08-26 14:14:?? Wen.");
+        String dataString1 = "Alipay,Wechatpay,";              //[Alipay, Wechatpay]
+        String dataString2 = "Alipay,Wechatpay,,";             //[Alipay, Wechatpay]
+        String dataString3 = "Alipay,Wechatpay,,,";            //[Alipay, Wechatpay]
+        String dataString4 = "Alipay,Wechatpay, ,";            //[Alipay, Wechatpay,  ]              //" "
+        String dataString5 = "Alipay,Wechatpay, ,Unionpay";    //[Alipay, Wechatpay,  , Unionpay]    //" "
+        String dataString6 = ",Alipay,Wechatpay";              //[, Alipay, Wechatpay]               //""
+        String dataString7 = ",,Alipay,Wechatpay";             //[, , Alipay, Wechatpay]             //两个都是 ""
+        String dataString8 = ",,,Alipay,Wechatpay";            //[, , , Alipay, Wechatpay]           //三个都是 ""
+        String[] dataArray = dataString8.split(",");
+        System.out.println("==" + dataArray[2] + "==");
+        List<String> dataList = Arrays.asList(dataArray);
+        System.out.println(dataList);
+        /**
+         *当分割符号(,)在字符串首时生效，会把分割符号前的默认解析成空字符串("" ,不是null)   (有几个分隔符解析几个空串"")
+         *当分割符号(,)在字符串尾时失效，会把分割符号后的舍弃掉  (无论多少个分隔符)
+         * */
+        String empty = "";
+        System.out.println("==" + empty + "==");
+
     }
 }
