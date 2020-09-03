73f0c6317921c80ac5ca3fdfb4f66076ba4b66a9
Allen.C.Y.Zhang <allenzhang@oceanpayment.com.cn>
2020-09-03 16:45:41 +0800

    BigDecimal 的装箱 , String   和  double

--- src/main/java/com/zcy/tools/decimal_format/BigDecimal2String.java
+++ src/main/java/com/zcy/tools/decimal_format/BigDecimal2String.java
@@ -8,12 +8,30 @@ public class BigDecimal2String {
 
     public static void main(String[] args) {
 
-        BigDecimal bigDecimal = new BigDecimal(1233.12);
+        BigDecimal bigDecimal = new BigDecimal(1233.128800);
+
+        BigDecimal bigDecimal2 = new BigDecimal("1233.128800");
+
+        BigDecimal bigDecimal3 = BigDecimal.valueOf(1233.128800);
+
+        BigDecimal bigDecimal4 = new BigDecimal(Double.valueOf(1233.128800).toString());
+
 
         System.out.println(bigDecimal);
+        System.out.println(bigDecimal2);
+        System.out.println(bigDecimal3);
+        System.out.println(bigDecimal4);
 
-        System.out.println(bigDecimal.toString());
+//        System.out.println(bigDecimal.toString());
+//        System.out.println(bigDecimal2.toString());
 
-        List list = new ArrayList();
+        System.out.println("=================================================");
+        BigDecimal b1 = BigDecimal.valueOf(100.0);
+        BigDecimal b2 = BigDecimal.valueOf(0.01);
+        System.out.println(b1.equals(b2));
+        System.out.println(b1.compareTo(b2) == 0);
+
+
+        System.out.println(b1.multiply(b2));
     }
 }
