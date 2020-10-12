9c4e8a18fcbd1548596c0cc6afe70dfdebd67df1
Allen.C.Y.Zhang <allenzhang@oceanpayment.com.cn>
2020-10-12 20:55:31 +0800

    空指针if预防

--- src/main/java/com/zcy/string_test/NullPoninterTest.java
+++ src/main/java/com/zcy/string_test/NullPoninterTest.java
@@ -3,10 +3,17 @@ package com.zcy.string_test;
 public class NullPoninterTest {
     public static void main(String[] args) {
 
+
+        String ss = null;
+        if (ss != null && ss.equals("qwe")) {  // 不会空指针！
+            System.out.println("测试");
+        }
+
+
         String s = null;
 
-        System.out.println(s.length());
-
+        //System.out.println(s.length());
+        System.out.println("ok");
 
     }
 }
