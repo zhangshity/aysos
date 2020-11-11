d59371f54e315b2d79998ff99222530f046303f9
Allen.C.Y.Zhang <allenzhang@oceanpayment.com.cn>
2020-11-11 20:50:05 +0800

    String contains()  方法测试

---
+++ src/main/java/com/zcy/string_test/ContainTest.java
@@ -0,0 +1,14 @@
+package com.zcy.string_test;
+
+public class ContainTest {
+    public static void main(String[] args) {
+
+
+        String s = "Referred to transaction has not been found.";
+
+        System.out.println(s.contains("has not been found"));
+
+
+
+    }
+}
