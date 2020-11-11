c531bfa299dbbd15e2c19191417ef166f7adb5b1
Allen.C.Y.Zhang <allenzhang@oceanpayment.com.cn>
2020-11-11 20:51:55 +0800

    ***  HashMap扩容测试 ！！！！ 超级干货  阳哥杰作

--- src/main/java/com/zcy/collection/map/HashMap_put.java
+++ src/main/java/com/zcy/collection/map/HashMap_put.java
@@ -1,10 +1,13 @@
 package com.zcy.collection.map;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 public class HashMap_put {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, NoSuchFieldException, InvocationTargetException, IllegalAccessException {
 
         //HashMap的key和value均可为null ,(key只能哟一个null因为不能重复)
 
@@ -32,16 +35,42 @@ public class HashMap_put {
 
 
 
+        //================================= capacity 容量测试 =====================================================
+        System.out.println("\n\n\n================================= capacity 容量测试 =====================================================");
+        Map<String, String> hashMap = new HashMap<>(2);   //容量的初始值直接设置为 = 数据量大小/负载因子 +1
+        hashMap.put("key1", "value1");
+        //hashMap.put("key2", "value2");
+        //hashMap.put("key3", "value3");
+        //hashMap.put("key4", "value4");
+        //hashMap.put("key5", "value5");
+        //hashMap.put("key6", "value6");
 
 
+        Class<?> clazz = hashMap.getClass();
 
+        //数组容量
+        Method method = clazz.getDeclaredMethod("capacity");
+        method.setAccessible(true);
+        System.out.println("capacity : " + method.invoke(hashMap) + "      数组容量");
 
+        //负载因子(默认0.75)
+        Field field1 = clazz.getDeclaredField("loadFactor");
+        field1.setAccessible(true);
+        System.out.println("loadFactor : " + field1.get(hashMap) + "      负载因子(默认0.75)");
 
+        //扩容阈值(大于此数字才扩容)
+        Field field2 = clazz.getDeclaredField("threshold");
+        field2.setAccessible(true);
+        System.out.println("threshold : " + field2.get(hashMap) + "      扩容阈值(大于此数字才扩容)");
 
-        //===============HashTable========================================
-        Map<String, Integer> map1 = new Hashtable<>();
+        //容器内实际数据量
+        Field field4 = clazz.getDeclaredField("size");
+        field4.setAccessible(true);
+        System.out.println("size : " + field4.get(hashMap) + "      容器内实际数据量");
 
-
+        //Field field3 = clazz.getDeclaredField("table");
+        //field3.setAccessible(true);
+        //System.out.println("table.size : " + field3.get(hashMap));
     }
 
 }
