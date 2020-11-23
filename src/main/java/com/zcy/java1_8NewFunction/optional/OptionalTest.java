4a345ad0fa788b93cd548c56957c8952baf95228
Allen.C.Y.Zhang <allenzhang@oceanpayment.com.cn>
2020-11-23 14:09:55 +0800

    Optional 测试使用 (仅包含of()、ofNullable()、orElse()、orElseGet()  其他后续补充)

--- src/main/java/com/zcy/java1_8NewFunction/optional/OptionalTest.java
+++ src/main/java/com/zcy/java1_8NewFunction/optional/OptionalTest.java
@@ -1,2 +1,76 @@
-package com.zcy.java1_8NewFunction;public class OptionalTest {
+package com.zcy.java1_8NewFunction.optional;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+/**
+ * 关于Java1.8新功能Optional的测试
+ */
+public class OptionalTest {
+    public static void main(String[] args) {
+
+        /**
+         * 定义:
+         * Optional 为Java1.8提供的一个包装类,结构很简单,就一个<T>泛型成员变量value存储值。
+         * private构造函数,常用初始化静态方法: of() ofNullable() empty() ,顾名思义为赋值(),赋可为空的值(),赋空值()
+         */
+        Integer i = 10;
+
+        Integer i1 = Optional.of(i).get();
+        System.out.println(i1);
+
+        Integer i2 = Optional.ofNullable(i).get();
+        System.out.println(i2);
+
+        //Object o3 = Optional.empty().get();  //Exception in thread "main" java.util.NoSuchElementException: No value present
+        //System.out.println(o3);              //	at java.util.Optional.get(Optional.java:135)
+        //	at com.zcy.java1_8NewFunction.optional.OptionalTest.main(OptionalTest.java:18)
+
+
+
+        //================= 常用方法 orElse() 和 orElseGet() 测试 =================
+        /**
+         * 附区别帖子
+         * https://stackoverflow.com/questions/33170109/difference-between-optional-orelse-and-optional-orelseget#
+         *
+         * orElse(T other)无论前面Optional容器是null还是non-null，都会执行orElse里的方法，
+         * orElseGet(Supplier<? extends T> other)并不会，只有Optional容器是null才会执行orElseGet中的方法
+         *
+         * 如果service无异常抛出的情况下，Optional使用orElse或者orElseGet的返回结果都是一样的
+         *
+         *
+         * 差异的主要原因就在于参数里的Supplier接口,
+         * ！！## (只有消费了才会调用, 消费动作对应源码里的三目运算符最后一项,即Optional为空,才去消费,不为空不消费故不会调用)
+         */
+        System.out.println("================= 常用方法 orElse() 和 orElseGet() 测试 =================");
+        String s = "nonNull value";
+        //String s = null;
+
+        String s1 = Optional.ofNullable(s).orElse(OptionalTest.fetch("other value"));
+        System.out.println(s1);
+
+        String s2 = Optional.ofNullable(s).orElseGet(() -> OptionalTest.fetch("other value lambda")); //@FunctionalInterface lambda表达式写法
+        System.out.println(s2);
+
+        String s3 = Optional.ofNullable(s).orElseGet(new Supplier<String>() { //1.8前旧式接口重写写法(与上面的lambda无区别，参考理解)
+            @Override
+            public String get() {
+                //return "other value override";
+                return OptionalTest.fetch("other value override");
+            }
+        });
+        System.out.println(s3);
+    }
+
+
+    /**
+     * 自定义静态方法控制台打印参数值,并返回参数值
+     * @param s
+     * @return
+     */
+    private static String fetch(String s) {
+        System.out.println("Supplier()内部实现方法: 参数:" + s + "   ：~~已执行~~");
+        return s;
+    }
+
 }
