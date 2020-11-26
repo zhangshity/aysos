0fcaf93d9da15e7f1a86d85aa8603305ee3ace94
chunyang.zhang <525768181@qq.com>
2020-11-27 01:55:03 +0800

    Jedis弱智测试

---
+++ src/main/java/com/zcy/redis/JedisTest.java
@@ -0,0 +1,14 @@
+package com.zcy.redis;
+
+import redis.clients.jedis.Jedis;
+
+public class JedisTest {
+    public static void main(String[] args) {
+
+        Jedis jedis = new Jedis("localhost");
+        jedis.set("god", "zcy");
+
+        System.out.println("redis 获取值: " + jedis.get("god"));
+
+    }
+}
