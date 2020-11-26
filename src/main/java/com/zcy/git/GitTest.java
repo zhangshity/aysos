61c1638a1112335ddd8444d70e2b87228ddf794b
Allen.C.Y.Zhang <allenzhang@oceanpayment.com.cn>
2020-11-26 11:02:25 +0800

    10

--- src/main/java/com/zcy/git/GitTest.java
+++ src/main/java/com/zcy/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("第6次提交");
         System.out.println("第7次提交");
         System.out.println("第8次提交");
+        System.out.println("第10次提交");
     }
 }
