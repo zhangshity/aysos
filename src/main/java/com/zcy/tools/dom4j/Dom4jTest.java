497d0ad9d302ba9312d4abf28d4af173884b2845
Allen.C.Y.Zhang <allenzhang@oceanpayment.com.cn>
2020-11-26 20:07:03 +0800

    dom4j + Optional

--- src/main/java/com/zcy/tools/dom4j/Dom4jTest.java
+++ src/main/java/com/zcy/tools/dom4j/Dom4jTest.java
@@ -5,6 +5,10 @@ import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class Dom4jTest {
     public static void main(String[] args) throws DocumentException {
         String xml = "<xml>\n" +
@@ -49,6 +53,104 @@ public class Dom4jTest {
         System.out.println(returnMsg);
 
 
+
+        //=========================================================测试2===============================
+        String xml2 = "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
+                "    <SOAP-ENV:Header/>\n" +
+                "    <SOAP-ENV:Body>\n" +
+                "        <ipgapi:IPGApiActionResponse xmlns:a1=\"http://ipg-online.com/ipgapi/schemas/a1\"\n" +
+                "            xmlns:ipgapi=\"http://ipg-online.com/ipgapi/schemas/ipgapi\"\n" +
+                "            xmlns:v1=\"http://ipg-online.com/ipgapi/schemas/v1\">\n" +
+                "            <ipgapi:successfully>true</ipgapi:successfully>\n" +
+                "            <ipgapi:OrderId>201126165957910285014</ipgapi:OrderId>\n" +
+                "            <v1:Billing>\n" +
+                "                <v1:Name>Poyi Liao</v1:Name>\n" +
+                "                <v1:MobilePhone>NULL</v1:MobilePhone>\n" +
+                "            </v1:Billing>\n" +
+                "            <v1:Shipping/>\n" +
+                "            <a1:TransactionValues>\n" +
+                "                <v1:CreditCardTxType>\n" +
+                "                    <v1:Type>payer auth</v1:Type>\n" +
+                "                </v1:CreditCardTxType>\n" +
+                "                <v1:CreditCardData>\n" +
+                "                    <v1:CardNumber>528946...0546</v1:CardNumber>\n" +
+                "                    <v1:ExpMonth>09</v1:ExpMonth>\n" +
+                "                    <v1:ExpYear>25</v1:ExpYear>\n" +
+                "                    <v1:Brand>MASTERCARD</v1:Brand>\n" +
+                "                </v1:CreditCardData>\n" +
+                "                <v1:Payment>\n" +
+                "                    <v1:ChargeTotal>1864</v1:ChargeTotal>\n" +
+                "                    <v1:Currency>344</v1:Currency>\n" +
+                "                </v1:Payment>\n" +
+                "                <v1:TransactionDetails>\n" +
+                "                    <v1:OrderId>201126165957910285014</v1:OrderId>\n" +
+                "                    <v1:MerchantTransactionId>201126165957910285014</v1:MerchantTransactionId>\n" +
+                "                    <v1:TDate>1606381219</v1:TDate>\n" +
+                "                    <v1:TransactionOrigin>ECI</v1:TransactionOrigin>\n" +
+                "                </v1:TransactionDetails>\n" +
+                "                <ipgapi:IPGApiOrderResponse>\n" +
+                "                    <ipgapi:ApprovalCode>?:waiting 3dsecure</ipgapi:ApprovalCode>\n" +
+                "                    <ipgapi:Brand>MASTERCARD</ipgapi:Brand>\n" +
+                "                    <ipgapi:Country>HKG</ipgapi:Country>\n" +
+                "                    <ipgapi:OrderId>201126165957910285014</ipgapi:OrderId>\n" +
+                "                    <ipgapi:IpgTransactionId>69819415276</ipgapi:IpgTransactionId>\n" +
+                "                    <ipgapi:PaymentType>CREDITCARD</ipgapi:PaymentType>\n" +
+                "                    <ipgapi:ReferencedTDate>1606381219</ipgapi:ReferencedTDate>\n" +
+                "                    <ipgapi:TDate>1606381219</ipgapi:TDate>\n" +
+                "                    <ipgapi:TDateFormatted>2020.11.26 14:30:19 (IST)</ipgapi:TDateFormatted>\n" +
+                "                </ipgapi:IPGApiOrderResponse>\n" +
+                "                <a1:Brand>MASTERCARD</a1:Brand>\n" +
+                "                <a1:TransactionType>PAYER_AUTH</a1:TransactionType>\n" +
+                "                <a1:TransactionState>WAITING_3D_SECURE</a1:TransactionState>\n" +
+                "                <a1:UserID>1</a1:UserID>\n" +
+                "                <a1:SubmissionComponent>API</a1:SubmissionComponent>\n" +
+                "            </a1:TransactionValues>\n" +
+                "        </ipgapi:IPGApiActionResponse>\n" +
+                "    </SOAP-ENV:Body>\n" +
+                "</SOAP-ENV:Envelope>";
+
+
+        String bankResponseCode = null;
+        String bankTxStatus = null;
+        String bankTxAmount = null;
+        String bankTxCurrencyCode = null;
+        String tranReturnCode = null;
+        String tranInfo = null;
+        //==================
+        Document document2 = DocumentHelper.parseText(xml2);
+        Element root2 = document2.getRootElement();
+        Element ipgApiActionResponse = Optional.ofNullable(root2.element("Body")).map(x -> x.element("IPGApiActionResponse")).orElse(null);
+        // 银行响应码
+        bankResponseCode = Optional.ofNullable(ipgApiActionResponse).map(x -> x.elementText("successfully")).orElse(null);
+        if ("true".equals(bankResponseCode)) {
+            List<Element> transactionValuesList = ipgApiActionResponse.elements("TransactionValues");
+            // 判断报文TransactionValues节点,是否有多条数据,如为多条取最后一条的数据
+            if (transactionValuesList != null && transactionValuesList.size() > 1) {
+                bankTxStatus = transactionValuesList.get(transactionValuesList.size() - 1).elementText("TransactionState");
+                bankTxAmount = Optional.ofNullable(transactionValuesList.get(transactionValuesList.size() - 1).element("Payment")).map(x -> x.elementText("ChargeTotal")).orElse(null);
+                bankTxCurrencyCode = Optional.ofNullable(transactionValuesList.get(transactionValuesList.size() - 1).element("Payment")).map(x -> x.elementText("Currency")).orElse(null);
+                // ApprovalCode':'分隔,数组长度>=3,tranReturnCode取前两个,tranInfo取剩余的,数组长度<3直接tranReturnCode/info均取原始值
+                List<String> approvalCodeList = Optional.ofNullable(transactionValuesList.get(transactionValuesList.size() - 1).element("IPGApiOrderResponse")).map(x -> x.elementText("ApprovalCode")).map(x -> Arrays.asList(x.split(":"))).orElse(null);
+                tranReturnCode = approvalCodeList != null && approvalCodeList.size() >= 3 ? approvalCodeList.get(0) + ":" + approvalCodeList.get(1) : approvalCodeList != null ? String.join(":", approvalCodeList) : null;
+                tranInfo = approvalCodeList != null && approvalCodeList.size() >= 3 ? String.join(":", approvalCodeList.subList(2, approvalCodeList.size())) : approvalCodeList != null ? String.join(":", approvalCodeList) : null;
+            } else if (transactionValuesList != null && transactionValuesList.size() == 1) {
+                bankTxStatus = transactionValuesList.get(0).elementText("TransactionState");
+                bankTxAmount = Optional.ofNullable(transactionValuesList.get(0).element("Payment")).map(x -> x.elementText("ChargeTotal")).orElse(null);
+                bankTxCurrencyCode = Optional.ofNullable(transactionValuesList.get(0).element("Payment")).map(x -> x.elementText("Currency")).orElse(null);
+                // ApprovalCode':'分隔,数组长度>=3,tranReturnCode取前两个,tranInfo取剩余的,数组长度<3直接tranReturnCode/info均取原始值
+                List<String> approvalCodeList = Optional.ofNullable(transactionValuesList.get(0).element("IPGApiOrderResponse")).map(x -> x.elementText("ApprovalCode")).map(x -> Arrays.asList(x.split(":"))).orElse(null);
+                tranReturnCode = approvalCodeList != null && approvalCodeList.size() >= 3 ? approvalCodeList.get(0) + ":" + approvalCodeList.get(1) : approvalCodeList != null ? String.join(":", approvalCodeList) : null;
+                tranInfo = approvalCodeList != null && approvalCodeList.size() >= 3 ? String.join(":", approvalCodeList.subList(2, approvalCodeList.size())) : approvalCodeList != null ? String.join(":", approvalCodeList) : null;
+            }
+        }
+
+        System.out.println("========================================");
+        System.out.println(bankResponseCode);
+        System.out.println(bankTxStatus);
+        System.out.println(bankTxAmount);
+        System.out.println(bankTxCurrencyCode);
+        System.out.println(tranReturnCode);
+        System.out.println(tranInfo);
 
 
 
