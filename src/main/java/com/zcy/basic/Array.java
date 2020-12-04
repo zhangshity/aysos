5c2964253c0f6e7801ae2e756b6540f997f32d16
Allen.C.Y.Zhang <allenzhang@oceanpayment.com.cn>
2020-12-04 09:31:05 +0800

    Array序列化测试

--- src/main/java/com/zcy/basic/Array.java
+++ src/main/java/com/zcy/basic/Array.java
@@ -1,5 +1,8 @@
 package com.zcy.basic;
 
+
+import java.util.Arrays;
+
 public class Array {
     public static void main(String[] args) {
         int[] i = new int[5]; //不符值默认都是零
@@ -14,6 +17,18 @@ public class Array {
         for (int value : i) {
             System.out.println(value);
         }
+
+
+        //============================ byte[] 和 String的转换测试 ====================================
+        System.out.println("============================ byte[] 和 String的转换测试 ====================================");
+        //序列化
+        String serial = "Hi,123q@";
+        byte[] bytes = serial.getBytes();
+
+        System.out.println(bytes); //[B@bc92429f  //数组无toString()方法,默认打印hashcode
+        System.out.println(Arrays.toString(bytes)); //[72, 105, 44, 49, 50, 51, 113, 64]
+        System.out.println(new String(bytes));
+
     }
 
 
