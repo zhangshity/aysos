b5bbc04593e8f134bb0c5d531cad91234ebefe45
Allen.C.Y.Zhang <allenzhang@oceanpayment.com.cn>
2020-12-14 20:01:49 +0800

    AOP自我调用 - 加入测试建表语句

--- src/main/java/com/zcy/spring/transaction/Test.java
+++ src/main/java/com/zcy/spring/transaction/Test.java
@@ -9,6 +9,16 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan("com.zcy.spring.transaction")
 public class Test {
 
+    // 测试表语句 (附加快速清理表数据语句: truncate table student)
+    // 建表语句:
+    /*
+        CREATE TABLE student (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            name varchar(255) DEFAULT NULL,
+            age int(11) DEFAULT NULL,
+            PRIMARY KEY (id)
+        )
+    */
 
 
     public static void main(String[] args) {
