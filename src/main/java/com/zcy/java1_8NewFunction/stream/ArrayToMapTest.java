098b286bb792e335a2435a98da0c795322e0c4d7
chunyang.zhang <525768181@qq.com>
2021-01-02 20:25:26 +0800

    stream list to map

--- src/main/java/com/zcy/java1_8NewFunction/stream/ArrayToMapTest.java
+++ src/main/java/com/zcy/java1_8NewFunction/stream/ArrayToMapTest.java
@@ -5,43 +5,48 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ArrayToMapTest {
     public static void main(String[] args) throws UnsupportedEncodingException {
 
 
-        String bankResponse = "K=08c30b1976142106e8db35db5da91cd7&\n" +
-                "M=10018949&\n" +
-                "O=101&\n" +
-                "T=11%2F24%2F2020+08%3A50%3A13&\n" +
-                "_M=10018949&\n" +
-                "_O=1&\n" +
-                "_a1=201124161441175128882&\n" +
-                "_a2=2&\n" +
-                "_a4=17745&\n" +
-                "_a5=USD&\n" +
-                "_a9=6&\n" +
-                "_b1=DF1773...4465&\n" +
-                "_b2=1&_b3=09&\n" +
-                "_b4=24&\n" +
-                "_i2=gtracing*OnlinShop&\n" +
-                "_z1=XZZ66f16a87173b2001GS4EWJN5YJHGR&\n" +
-                "_z13=032946765145&\n" +
-                "_z2=0&\n" +
-                "_z3=Transaction+has+been+executed+successfully.&\n" +
-                "_z4=111475&\n" +
-                "_z55=XZZ66f16a87173b2001DCZFZXEC6JGPP&\n" +
-                "_z6=00&\n" +
-                "a1=1606207812839&\n" +
-                "g4=201128888441175122602&\n" +
-                "z1=XZZ68caee536c749612JEHYXMATBFDL5&\n" +
-                "z2=0&\n" +
+        String bankResponse = "K=08c30b1976142106e8db35db5da91cd7&" +
+                "M=10018949&" +
+                "O=101&" +
+                "T=11%2F24%2F2020+08%3A50%3A13&" +
+                "_M=10018949&" +
+                "_O=1&" +
+                "_a1=201124161441175128882&" +
+                "_a2=2&" +
+                "_a4=17745&" +
+                "_a5=USD&" +
+                "_a9=6&" +
+                "_b1=DF1773...4465&" +
+                "_b2=1&_b3=09&" +
+                "_b4=24&" +
+                "_i2=gtracing*OnlinShop&" +
+                "_z1=XZZ66f16a87173b2001GS4EWJN5YJHGR&" +
+                "_z13=032946765145&" +
+                "_z2=0&" +
+                "_z3=Transaction+has+been+executed+successfully.&" +
+                "_z4=111475&" +
+                "_z55=XZZ66f16a87173b2001DCZFZXEC6JGPP&" +
+                "_z6=00&" +
+                "a1=1606207812839&" +
+                "g4=201128888441175122602&" +
+                "z1=Father.Yang-XZZ68caee536c749612JEHYXMATBFDL5&" +
+                "z2=0&" +
                 "z3=Transaction+has+been+executed+successfully.";
 
 
-        Map<String, String> bankResponseFieldMap = new HashMap<>(64);
+        System.out.println("========================= forEach遍历添加 =========================");
+        Map<String, String> bankResponseFieldMap = new ConcurrentHashMap<>(64);
 
         Arrays.stream(URLDecoder.decode(bankResponse, StandardCharsets.UTF_8.name()).split("&"))
                 .parallel()
@@ -50,11 +55,29 @@ public class ArrayToMapTest {
                     bankResponseFieldMap.put(kvArr[0].trim(), kvArr[1].trim());
                 });
 
-
-//                .map(x -> x.split("="))
-//                .collect(Collectors.toMap(k -> k[0], v -> v[1]));
-
         System.out.println(bankResponseFieldMap);
         System.out.println(bankResponseFieldMap.get("z1"));
+
+
+        System.out.println("========================= 普通map映射 =========================");
+        Map<String, String> bankResponseFieldMap1 = Arrays.stream(URLDecoder.decode(bankResponse, StandardCharsets.UTF_8.name()).split("&"))
+                .parallel()
+                .map(s -> s.split("="))
+                .collect(Collectors.toMap(s -> s[0].trim(), s -> s[1].trim()));
+//                .forEach(x -> System.out.printf(Arrays.toString(x)));
+        System.out.println(bankResponseFieldMap1);
+        System.out.println(bankResponseFieldMap1.get("z1"));
+
+
+        System.out.println("========================= flatMap =========================");
+        List<String> list = Arrays.stream(URLDecoder.decode(bankResponse, StandardCharsets.UTF_8.name()).split("&"))
+                .flatMap(s -> Arrays.stream(s.split("=")))
+                .collect(Collectors.toList());
+//                .forEach(System.out::println);
+        System.out.println(list);
+
+
+
+
     }
 }
