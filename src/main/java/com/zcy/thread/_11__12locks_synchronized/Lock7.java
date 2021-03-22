8c310c216077c9c71e1cbcaa523f3650b19d9cb6
chunyang.zhang <525768181@qq.com>
2021-03-23 00:48:03 +0800

    整理 + volatile

--- src/main/java/com/zcy/thread/_11__12locks_synchronized/Lock7.java
+++ src/main/java/com/zcy/thread/_11__12locks_synchronized/Lock7.java
@@ -1,35 +1,34 @@
-package com.zcy.thread._9__12locks_synchronized;
+package com.zcy.thread._11__12locks_synchronized;
 
 import java.util.concurrent.TimeUnit;
 
 /**
  * @ Author: chunyang.zhang
- * @ Description: 《先打电话再发短信》不同对象,同一类锁，主线程顺序执行，先创建A线程,获取Student8类锁,执行,等待4秒
- *                                      ,睡1秒,                  创建B线程,获取student2对象锁,执行
+ * @ Description: 《先打电话再发短信》不同对象,同一类锁，主线程顺序执行，先创建A线程,获取Student7类锁,执行,等待4秒
+ *                                      ,睡1秒,                  创建B线程,获取Student7对象锁,执行
  * @ Date: Created in 17:02 2021/3/11
  * @ Modified: By:
  *
- * 不同对象，对象锁和类锁
+ * 同一对象，对象锁和类锁
  */
-public class Lock8 {
+public class Lock7 {
     public static void main(String[] args) throws InterruptedException {
-        Student8 student1 = new Student8();
-        Student8 student2 = new Student8();
+        Student7 student = new Student7();
 
         new Thread(() -> {
-            student2.msg();
+            student.msg();
         }, "A").start();
 
         TimeUnit.SECONDS.sleep(1);
 
         new Thread(()->{
-            student2.call();
+            student.call();
         }, "B").start();
 
     }
 }
 
-class Student8 {
+class Student7 {
     public static synchronized void msg() {
         try {
             TimeUnit.SECONDS.sleep(4);
