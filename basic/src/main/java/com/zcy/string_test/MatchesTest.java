8f104dc488a64da795b42e5f49f21fbc80acd309
chunyang_zhang <525768181@qq.com>
2021-05-26 10:20:22 +0800

    matches QA

--- basic/src/main/java/com/zcy/string_test/MatchesTest.java
+++ basic/src/main/java/com/zcy/string_test/MatchesTest.java
@@ -1,7 +1,13 @@
 package com.zcy.string_test;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
+
 public class MatchesTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         String s = "1asc";
         String s1 = "1desc";
@@ -14,7 +20,7 @@ public class MatchesTest {
         test(s1);
     }
 
-    private static void test(String str) {
+    private static void test(String str) throws ParseException {
         System.out.println("endsWith(\"asc\") " + str.endsWith("asc"));
         System.out.println("endsWith(\"desc\") " + str.endsWith("desc"));
         System.out.println("endsWith(\"asc\") ||endsWith(\"desc\") " + (str.endsWith("asc") || str.endsWith("desc")));
@@ -23,6 +29,31 @@ public class MatchesTest {
         System.out.println(str.matches("[0-9].*"));
         System.out.println(str.matches("[0-9][a-z].*"));
         System.out.println((str.matches("[0-9][a-z][a-z][a-z]") || str.matches("[0-9][a-z][a-z][a-z][a-z]")));
+
+
+
+        //=================================== Supp. 2021-05-25 ===================================
+
+        System.out.println("----------matches日期1： " + "2021-05-25".matches("dddd-dd-dd"));
+        System.out.println("----------matches日期2： " + "2021-05-25".matches("[0-9]{4}[-][0-9]{2}[-][0-9]{2}"));
+        System.out.println("----------matches日期2： " + "2021-05-5".matches("\\d{4}[-]\\d{1,2}[-]\\d{1,2}"));
+
+
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd").parse("2021-5-05"));
+
+
+//        Date date = Optional.ofNullable("2021-05-25")
+//                .filter(acceptorDate -> acceptorDate.matches("dddd-dd-dd"))
+//                .map(x -> {
+//                    try {
+//                        return new SimpleDateFormat("yyyy-MM-dd").parse(x);
+//                    } catch (ParseException e) {
+//                        e.printStackTrace();
+//                    }
+//                })
+//                .orElse(null);
+//
+//        System.out.println(date);
 
     }
 }
