6fb1b83c859320ddc7112d35bbc557e7c742bfde
chunyang.zhang <525768181@qq.com>
2021-07-22 02:16:16 +0800

    camel-springboot

---
+++ fk-camel-springboot/src/main/java/com/zcy/FkCamelSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.zcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FkCamelSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FkCamelSpringbootApplication.class, args);
+	}
+
+}
---
+++ fk-camel-springboot/src/test/java/com/zcy/FkCamelSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zcy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FkCamelSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
