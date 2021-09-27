902966f7923867ae565912cac65c1a797c2bce39
zhangshity <37323793+zhangshity@users.noreply.github.com>
2021-09-27 17:41:03 +0800

    no message

--- fk-api-test/src/main/java/com/shit/springboottestcontroller/controller/AbstractController.java
+++ fk-api-test/src/main/java/com/shit/springboottestcontroller/controller/AbstractController.java
@@ -1,6 +1,5 @@
 package com.shit.springboottestcontroller.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
