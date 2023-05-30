75f82256ef4647eaefa6ef13fec630db2fc75242
Allen.C.Y.Zhang <zhangcy7@infore.com>
2023-05-30 18:18:39 +0800

    collection2

---
+++ basic/src/main/java/com/zcy/collection/IteratorTest.java
@@ -0,0 +1,58 @@
+package com.zcy.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class IteratorTest {
+    public static void main(String[] args) {
+
+        ReverseList<String> list = new ReverseList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        for (String s : list) {
+            System.out.println(s); // 3 2 1
+        }
+
+    }
+
+    static class ReverseList<T> implements Iterable<T> {
+
+        private final List<T> FINAL_LIST = new ArrayList<>();
+
+        public void add(T t) {
+            FINAL_LIST.add(t);
+        }
+
+        @Override
+        public Iterator iterator() {
+            return new Iterator() {
+                int index = FINAL_LIST.size();
+
+                @Override
+                public boolean hasNext() {
+                    return index > 0;
+                }
+
+                @Override
+                public Object next() {
+                    index--;
+                    return FINAL_LIST.get(index);
+                }
+            };
+        }
+
+        @Override
+        public void forEach(Consumer action) {
+            Iterable.super.forEach(action);
+        }
+
+        @Override
+        public Spliterator spliterator() {
+            return Iterable.super.spliterator();
+        }
+    }
+}
