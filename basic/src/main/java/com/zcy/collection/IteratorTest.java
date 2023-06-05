656bc044c84559792e76b9eaf0726c16ae99a723
Allen.C.Y.Zhang <zhangcy7@infore.com>
2023-06-05 14:09:24 +0800

    Update IteratorTest.java

--- basic/src/main/java/com/zcy/collection/IteratorTest.java
+++ basic/src/main/java/com/zcy/collection/IteratorTest.java
@@ -8,7 +8,7 @@ import java.util.function.Consumer;
 
 public class IteratorTest {
     public static void main(String[] args) {
-
+        //Java编译器并不知道如何遍历List。上述代码能够编译通过，只是因为编译器把for each循环通过Iterator改写为了普通的for循环：
         ReverseList<String> list = new ReverseList<>();
         list.add("1");
         list.add("2");
@@ -18,41 +18,43 @@ public class IteratorTest {
         }
 
     }
+}
 
-    static class ReverseList<T> implements Iterable<T> {
 
-        private final List<T> FINAL_LIST = new ArrayList<>();
+class ReverseList<T> implements Iterable<T> {
 
-        public void add(T t) {
-            FINAL_LIST.add(t);
-        }
+    private final List<T> FINAL_LIST = new ArrayList<>();
 
-        @Override
-        public Iterator iterator() {
-            return new Iterator() {
-                int index = FINAL_LIST.size();
+    public void add(T t) {
+        FINAL_LIST.add(t);
+    }
 
-                @Override
-                public boolean hasNext() {
-                    return index > 0;
-                }
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            int index = FINAL_LIST.size();
 
-                @Override
-                public Object next() {
-                    index--;
-                    return FINAL_LIST.get(index);
-                }
-            };
-        }
+            @Override
+            public boolean hasNext() {
+                return index > 0;
+            }
 
-        @Override
-        public void forEach(Consumer action) {
-            Iterable.super.forEach(action);
-        }
+            @Override
+            public Object next() {
+                index--;
+                return FINAL_LIST.get(index);
+            }
+        };
+    }
 
-        @Override
-        public Spliterator spliterator() {
-            return Iterable.super.spliterator();
-        }
+    @Override
+    public void forEach(Consumer action) {
+        Iterable.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator spliterator() {
+        return Iterable.super.spliterator();
     }
 }
+
